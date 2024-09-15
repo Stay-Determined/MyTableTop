1e03a7aa084f6f5115af5c522a31a445dd23de58
Antoine TACAIL--CASES <74400261+Stay-Determined@users.noreply.github.com>
2024-09-15 23:03:59 +0200

    add draw, shuffle and end to draw

--- src/components/createdComponents/Deck/index.js
+++ src/components/createdComponents/Deck/index.js
@@ -5,22 +5,29 @@ import clover from "../../../assets/Cards/clover";
 import diamond from "../../../assets/Cards/diamond";
 import heart from "../../../assets/Cards/heart";
 import deck from "../../../assets/Deck/back_deck.png"
-
+import draw from "../../../assets/Deck/draw.png"
+import shuffle from "../../../assets/Deck/shuffle.png"
+import arrayShuffle from 'array-shuffle';
 
 const Deck = () => {
-  var listeCarte = ["1s","2s","3s","4s","5s","6s","7s","8s","9s","10s","11s","12s","13s",
+  var cartesDeck = ["1s","2s","3s","4s","5s","6s","7s","8s","9s","10s","11s","12s","13s",
               "1c","2c","3c","4c","5c","6c","7c","8c","9c","10c","11c","12c","13c",
               "1d","2d","3d","4d","5d","6d","7d","8d","9d","10d","11d","12d","13d",
               "1h","2h","3h","4h","5h","6h","7h","8h","9h","10h","11h","12h","13h"];
   var image = null;
   var famille = null;
   const [carte, setCarte] = useState(null);
-  const [numCarte,setNumCarte] = useState(0);
+  
+  const [listeCarte, setListeCarte] = useState(cartesDeck);
 
+  function melangerDeck() {
+    setListeCarte(arrayShuffle(listeCarte));
+  }
+  
 
   function pioche(){
     
-    switch (listeCarte[numCarte].slice(-1)) {
+    switch (listeCarte[0].slice(-1)) {
       case "s":
         famille = spade;
         break;
@@ -36,8 +43,8 @@ const Deck = () => {
       default:
         famille = "none";
       }
-      console.log("famille = "+famille);
-      switch (listeCarte[numCarte].slice(0,-1)) {
+
+      switch (listeCarte[0].slice(0,-1)) {
         case "1":
           image = famille.img1;
           break;
@@ -94,16 +101,22 @@ const Deck = () => {
           image = "none";
           break;
   };
-  console.log("image = "+image)
-  setCarte(image);
-  setNumCarte(numCarte + 1);
   
-
+  setCarte(image);
+  listeCarte.shift();
   };
+
 
  return (
   <div>
-    <img src={deck} alt="card" style={{ margin: "auto", cursor : "pointer" }} onClick={pioche}/>
+    
+     {listeCarte.length > 0 &&
+     (<div>
+          <img src={shuffle} alt="shuffle" style={{ margin: "auto", cursor : "pointer" }} onClick={melangerDeck} />
+          <img src={deck} alt="card"/>
+          <img src={draw} alt="draw" style={{ margin: "auto", cursor : "pointer" }} onClick={pioche} />
+      </div>)}
+    
     {carte && <img src={carte} alt="card" style={{ margin: "auto" }} />}
   
    
